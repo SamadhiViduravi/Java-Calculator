06e62784d43141e2164b60f62ea741976251b80c
SamadhiViduravi <167635543+SamadhiViduravi@users.noreply.github.com>
2025-06-26 22:59:43 +0530

    error fixed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);  
+        Scanner myObj = new Scanner(System.in);
         System.out.println("Enter first value ");
-        int a = myObj.nextLine();
+        int a = myObj.nextInt();
         System.out.println("Enter second value ");
         int b = myObj.nextInt();
         Process ob1 = new Process(a,b);
