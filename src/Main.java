7a9fa453a6bd05088dcf09c5fb9c431d802afbce
SamadhiViduravi <167635543+SamadhiViduravi@users.noreply.github.com>
2025-06-26 22:56:05 +0530

    imported scanner class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        
         Process ob1 = new Process(2,4);
         ob1.add();
         ob1.sub();
