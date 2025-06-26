6c0b1a533df5d3aae17c9d7455a8324636294de4
SamadhiViduravi <167635543+SamadhiViduravi@users.noreply.github.com>
2025-06-26 22:57:31 +0530

    got user inputs

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
-        Process ob1 = new Process(2,4);
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+
+        int a = myObj.nextLine();
+        int b = myObj.nextInt();
+        Process ob1 = new Process(a,b);
         ob1.add();
         ob1.sub();
         ob1.mul();
